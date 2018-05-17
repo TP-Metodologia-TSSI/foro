fdc1f5c14f74294b78ad4a01263ebe119f94378d
Gian Franco Stabile <gianfranco.stabile98@gmail.com>
2018-05-16 21:44:56 -0300

    Views Added

---
+++ src/main/java/com/metodologia/foro/view/Views/PartsTemplate/header.php
@@ -0,0 +1,24 @@
+<?php  
+	namespace Vistas\PartsTemplate;	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Foro Temático</title>
+	<meta charset="utf-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="../../CSS/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../../CSS/styles.css">
+	<link rel="stylesheet" type="text/css" href="../../CSS/font-awesome.css">
+
+	<script src="../../JS/JQuery.js"></script>
+</head>
+
+<body class="bg-light position-relative min-h-100">
+
+	<?php 
+	require("/navbar.php");
+	?>
