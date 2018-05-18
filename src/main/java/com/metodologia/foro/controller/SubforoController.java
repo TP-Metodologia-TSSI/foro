b600515a7e43a83407de24741631c1078c2e6d10
ltoniut <ltoniut@gmail.com>
2018-05-18 19:42:52 -0300

    Nuevos controladores

--- src/main/java/com/metodologia/foro/controller/SubforoController.java
+++ src/main/java/com/metodologia/foro/controller/SubforoController.java
@@ -1,5 +1,45 @@
 package com.metodologia.foro.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.metodologia.foro.entities.Subforo;
+import com.metodologia.foro.response.LoginResponseWrapper;
+import com.metodologia.foro.utils.SessionData;
+
+@RestController
+@RequestMapping(
+    value = "/subforo",
+    produces = MediaType.APPLICATION_JSON_VALUE
+)
+
 public class SubforoController {
+
+	private SubforoService SubforoService;
+
+	private SessionData sessionData;
+
+	@Autowired
+	public SubforoController(SubforoService subforoService, SessionData sessionData) {
+        this.subforoService = subforoService;
+        this.sessionData = sessionData;
+	}
+
+	@RequestMapping("/")
+	public @ResponseBody ResponseEntity<SubforoResponseWrapper> getById(@RequestParam("id") Integer id) {
+		Subforo s = subforoService.getSubforo(id);
+		if (null != s) {
+            String sessionId = sessionData.addSession(s);
+            return new ResponseEntity<SubforoResponseWrapper>(new subforoResponseWrapper(sessionId), HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+	}
 
 }
--- src/main/java/com/metodologia/foro/controller/TemaController.java
+++ src/main/java/com/metodologia/foro/controller/TemaController.java
@@ -1,5 +1,43 @@
 package com.metodologia.foro.controller;
 
-public class TemaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.metodologia.foro.entities.Tema;
+import com.metodologia.foro.response.LoginResponseWrapper;
+import com.metodologia.foro.utils.SessionData;
+
+@RestController
+@RequestMapping(
+    value = "/tema",
+    produces = MediaType.APPLICATION_JSON_VALUE
+)
+
+public class TemaController {
+	private TemaService temaService;
+
+    private SessionData sessionData;
+
+	@Autowired
+	public TemaController(TemaService temaService, SessionData sessionData) {
+        this.temaService = temaService;
+        this.sessionData = sessionData;
+	}
+
+	@RequestMapping("/")
+    public @ResponseBody ResponseEntity<TemaResponseWrapper> getById(@RequestParam("id") Integer id) {
+        Tema t = temaService.getTema(id);
+        if (null != t) {
+            String sessionId = sessionData.addSession(t);
+            return new ResponseEntity<TemResponseWrapper>(new TemaResponseWrapper(sessionId), HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+	}
 }
